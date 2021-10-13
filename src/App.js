f2931901a00ee467c5f79252c15a9fb691454ad4
Prabhat Singh <prabhatsingh@Prabhats-MacBook-Pro.local>
2021-10-13 20:25:32 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Hello from './text/hello';
 
 function App() {
+  const [name, setName] = React.useState('Ishika');
+
+  React.useEffect(()=>{
+    console.log(name)
+  }, [])
+
+  const BtnClick = () => {
+    setName('Prabhat')
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     hii {name}
+     <Hello />
+     <Hello />
+     <button onClick={BtnClick}>On Click</button>
     </div>
   );
 }
